2ebe534ceb91d41adfdb30dc53d8509321cb3bc6
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-06-23 23:43:45 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -2,6 +2,7 @@
 # python function definitions are defined by the keyword def
 
 def multiply(x=7, y=9):  # function with two parameters, 7 and 9 are default values
+   # we don't put spaces after and before the equals sign when adding default values to our parameters
     result = x * y
     return result  # return keyword just like c++
 
@@ -53,18 +54,56 @@ prompt = "Please enter an even number"
 
 
 def get_even_int(prompt):
-     while True:
-         temp = int(input(prompt))  # remember python input is string by default
-         if temp % 2 == 0:
-              print("Correct")
-              return # without this, the while loop would continue indefinitely
-         else:
-           raise ValueError("{} is not an even integer".format(temp)) # raise keyword used to throw exceptions
-           #  print("Try again")
+    """
+    docstrings go inside the function in python, just before the code
+    they are an attribute of the function object
+
+    """
+    while True:
+        temp = int(input(prompt))  # remember python input is string by default
+        if temp % 2 == 0:
+            print("Correct")
+            return  # without this, the while loop would continue indefinitely
+        else:
+            raise ValueError("{} is not an even integer".format(temp))  # raise keyword used to throw exceptions
+            #  print("Try again")
 
 
-get_even_int(prompt)
+# get_even_int(prompt)
 
 # functions that do not explicitly return anything return None
 
 # we can force functions to crash under specific circumstances by raising an exception
+
+# there are also keyword arguments we can pass in, so we can explicitly define what a varialbe should be in the function
+print(multiply(y=27))  # y is passed in as a keyword argument
+
+
+# we typically pass in paratemers positionally, but we can also use default or keywords like the above
+
+# lets try creating a function to return the fibonacci number of a value passed in
+
+def fibo(n: int = 5) -> int: # annotate the function to show it accepts an integer and returns an integer
+    # so the default value of the fibo function is 5 and its function annotation shows it should be passed an int and returns an int
+    # adding default values within a function annotation requires us to put spaces before and after the equals sign
+    # remember that when we just want default values, we do not put spaces before or after the quals sign
+    """
+    return the nth fibonacci number for positive n
+    :param n:
+    :return: The fibonacci number
+    """
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    fib = [0, 1]  # a list with the first two numbers of the fibonacci sequence
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[-1]  # return the last element
+# function annotations make it clearer what kind of values your functions can accept and what they return
+
+print(fibo(5))
+print(fibo(15))
+
