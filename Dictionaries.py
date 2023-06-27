b9f022b2c5d695f2a7a2c79bf7dff1722d22a2a3
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-06-27 21:43:26 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,3 +1,4 @@
+
 # lists and tuples are sequence types, we can use indexes to access individual items in them
 # dictionaries and sets are not sequences, they are mappings,
 # dictionaries store key/value pairs, each va,ue has a unique key which is used to refer to it
@@ -40,3 +41,45 @@ for key, value in vehicles.items():
 # we change values in the dictionary by just assigning a new item to that key
 vehicles["Clio"] = "Sold!"
 # the insertion order is still preserved even though we've changed the value
+
+# we can delete from a dictionary using the del keyword
+del vehicles["Clio"]
+# accessing a dictionary with a key that doesnt exist will crash your program
+# this is why its safer to use the dictionary functions to add and delete itema,s as this won't crash if it fails
+# lists and dictionaries have a pop method
+vehicles.pop("Brexit", None) # None is the default value
+# the pop method removes the value associated with the key you pass
+# if the value does not exit, it returns the default value you pass in the second parameter
+# thats why the above pop method doesnt cause a crash
+result = vehicles.pop("Brexit", None)
+print(result)
+
+# lets create a menu for buying a computer using a dictionary
+
+available_parts = {"1" : "computer",
+                   "2" : "monitor",
+                   "3" : "keyboard",
+                   "4" : "mouse",
+                   "5" : "hdmi cable"
+                   }
+current_choice = None # create this variable and initialise it
+print(available_parts) # printing a dictionary like this prints the keys and values
+computer_parts = [] # empty list, its very common to cmobine lists and dictionaries
+while current_choice != "0":
+    if current_choice in available_parts:  # when you use the in keyword in a dictionary, we're only checking the keys
+        chosen_part = available_parts[current_choice]
+        if chosen_part in computer_parts:
+            print("removing {}".format(chosen_part))
+            computer_parts.remove(chosen_part)
+        else:
+            print(f"Adding {chosen_part} ") # f string, more compact than .format method
+            computer_parts.append(chosen_part)
+    else:
+        print("Not in the menu, please select:")
+      #  for i in available_parts:
+       #     print(i, " : ", available_parts[i])
+        # its more efficient to use the .items() function
+        for key, value in available_parts.items():
+            print(key, ":", value)
+
+    current_choice = input("Whats your choice? ")
